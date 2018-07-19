a3537838c0dc403f7ed0b2acb6b789d28b3551a4
apple <983669520@qq.c>
2018-07-19 21:24:56 +0800

    testgit

--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,4 @@
-"""user URL Configuration
+"""login URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import  url
+from login import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^index/',views.index),
+    url(r'^login/',views.index),
+    url(r'^register/',views.index),
+    url(r'^logout/',views.index),
+
 ]
